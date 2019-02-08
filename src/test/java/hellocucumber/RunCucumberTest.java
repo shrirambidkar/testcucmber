f33d8163bdbcaa9c994a4f91f25f92cc396679f7
shrirambidkar <shriram.bidkar@gmail.com>
2019-02-08 15:40:45 -0600

    Update RunCucumberTest.java

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"html:target/cucumber-html-report", "json:target/cucumber.json"})
 public class RunCucumberTest {
 }
